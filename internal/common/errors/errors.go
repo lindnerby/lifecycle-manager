7a2fb07113627c26c6da70b18cf0ec9d9b5af618
Benjamin Lindner <benjamin.lindner@sap.com>
2025-03-11 07:15:09 +0100

    merge in one file

--- internal/common/errors/errors.go
+++
@@ -1,8 +0,0 @@
-package errors
-
-import "errors"
-
-var (
-	ErrModulesManifestNameEmpty      = errors.New("manifest name is empty")
-	ErrModulesManifestNamespaceEmpty = errors.New("manifest namespace is empty")
-)
--- internal/usecase/kyma/status/modules/update.go
+++ internal/usecase/kyma/status/modules/update.go
@@ -8,36 +8,41 @@ import (
 	"github.com/kyma-project/lifecycle-manager/pkg/templatelookup/moduletemplateinfolookup"
 	"github.com/kyma-project/lifecycle-manager/pkg/util"
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
-	"github.com/kyma-project/lifecycle-manager/internal/pkg/metrics"
 	"github.com/kyma-project/lifecycle-manager/pkg/module/common"
 )
 
-type UpdateStatusModulesExisting interface {
-	Execute(ctx context.Context, kyma *v1beta2.Kyma, modules common.Modules) error
-}
+var (
+	errModulesManifestNameEmpty      = errors.New("manifest name is empty")
+	errModulesManifestNamespaceEmpty = errors.New("manifest namespace is empty")
+)
 
-type UpdateStatusModulesDeleted interface {
-	Execute(ctx context.Context, kyma *v1beta2.Kyma)
+type MetricsCleanupFunc func(kymaName, moduleName string)
+
+type ManifestClient interface {
+	Get(ctx context.Context, objectKey client.ObjectKey) (v1beta2.Manifest, error)
 }
 
 type UpdateStatusModulesUC struct {
-	updateExistingStatusModulesUC UpdateStatusModulesExisting
-	updateDeletedStatusModulesUC  UpdateStatusModulesDeleted
+	manifestClient ManifestClient
+	cleanupMetrics MetricsCleanupFunc
 }
 
-func NewUpdateStatusModulesUC() *UpdateStatusModulesUC {
-	return &UpdateStatusModulesUC{}
+func NewUpdateStatusModulesUC(cleanupMetrics MetricsCleanupFunc, manifestClient ManifestClient) *UpdateStatusModulesUC {
+	return &UpdateStatusModulesUC{
+		manifestClient: manifestClient,
+		cleanupMetrics: cleanupMetrics,
+	}
 }
 
 func (uc *UpdateStatusModulesUC) Execute(ctx context.Context, kyma *v1beta2.Kyma, modules common.Modules) error {
-	err := uc.updateExistingStatusModulesUC.Execute(ctx, kyma, modules)
-
 	updateModuleStatusFromExistingModules(kyma, modules)
-	deleteNoLongerExistingModuleStatus(ctx, kyma, r.getModule, kymaMetrics.RemoveModuleStateMetrics)
+	err := uc.deleteNoLongerExistingModuleStatus(ctx, kyma)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -150,4 +155,43 @@ func generateModuleStatusFromError(module *common.Module, existStatus *v1beta2.M
 			Message: module.Template.Err.Error(),
 		}
 	}
+}
+
+func (uc *UpdateStatusModulesUC) deleteNoLongerExistingModuleStatus(ctx context.Context, kyma *v1beta2.Kyma) error {
+	moduleStatuses := kyma.GetModuleStatusMap()
+	moduleStatusesToBeDeletedFromKymaStatus := kyma.GetNoLongerExistingModuleStatus()
+
+	for _, moduleStatus := range moduleStatusesToBeDeletedFromKymaStatus {
+		if moduleStatus.Manifest == nil {
+			uc.cleanupMetrics(kyma.Name, moduleStatus.Name)
+			delete(moduleStatuses, moduleStatus.Name)
+			continue
+		}
+		manifestName := moduleStatus.GetManifestName()
+		if manifestName.Name == "" {
+			return errModulesManifestNameEmpty
+		}
+		if manifestName.Namespace == "" {
+			return errModulesManifestNamespaceEmpty
+		}
+
+		manifest, err := uc.manifestClient.Get(ctx, manifestName)
+		if util.IsNotFound(err) {
+			uc.cleanupMetrics(kyma.Name, moduleStatus.Name)
+			delete(moduleStatuses, moduleStatus.Name)
+		} else {
+			moduleStatus.State = manifest.Status.State
+		}
+	}
+	kyma.Status.Modules = convertToNewModuleStatus(moduleStatuses)
+
+	return nil
+}
+
+func convertToNewModuleStatus(moduleStatusMap map[string]*v1beta2.ModuleStatus) []v1beta2.ModuleStatus {
+	newModuleStatus := make([]v1beta2.ModuleStatus, 0)
+	for _, moduleStatus := range moduleStatusMap {
+		newModuleStatus = append(newModuleStatus, *moduleStatus)
+	}
+	return newModuleStatus
 }
